e7af78adcbe6143d649099c1625f22811b3db22b
Woodrow Shen <woodrow.shen@canonical.com>
2017-06-07 10:24:04 +0800

    Add output list for rsync --include-from format

--- parse.py
+++ parse.py
@@ -2,15 +2,15 @@
 
 import os
 
-list=""
+list="RSYNC_INCLUDE=\""
 with open('package.list', 'w') as pkg,  open('output.list', 'w') as out:
-    with open('ubuntu.list', 'r', encoding='UTF-8') as file:
+    with open('md5-check', 'r', encoding='UTF-8') as file:
         for line in file:
-            if ".deb" not in line:
+            if ".deb" not in line and ".udeb" not in line:
                 continue
             else:
                 temp = line[:-1]
                 list = list + "--include " +  os.path.basename(temp) + " "
                 pkg.write(os.path.basename(line))
-    list += '\n'
+    list += '\"\n'
     out.write(list)
